5d0eba7485ee712f9557b4683a7795adf91d9dcc
Humenius <contact@humenius.me>
2020-06-15 12:11:49 +0200

    Add input check for keypad

--- src/keypaddoor.asm
+++ src/keypaddoor.asm
@@ -6,9 +6,9 @@ CSEG AT 100H
 ;		     INTERRUPT
 ; -------------------------------------------------
 
-ORG 0BH
-CALL timer
-RETI
+;ORG 0BH
+;CALL ENTRYPOINT
+;RETI
 
 ORG 20H
 INIT:	MOV IE, #10010010b
@@ -23,39 +23,45 @@ INIT:	MOV IE, #10010010b
 ; -------------------------------------------------
 ;		       LOGIC
 ; -------------------------------------------------
-timer:	 CALL DISPLAY
-         RET
+;ENTRYPOINT:	 CALL DISPLAY
+;		 CALL READIN
+;         	 RET
 
 ; -------------------------------------------------
 ;		DISPLAY CONTROLLING
 ;
 ;	  Interface for display controller
 ; -------------------------------------------------
-DISPLAY: mov P2, R2	; First block
-	 clr P1.0
-	 setb P1.0
+DISPLAY: MOV P2, R2	; First block
+	 CLR P1.0
+	 SETB P1.0
 
-	 mov P2, R3	; Second block
-	 clr P1.1
-  	 setb P1.1
+	 MOV P2, R3	; Second block
+	 CLR P1.1
+  	 SETB P1.1
 
-	 mov P2, R4	; Third block
-	 clr P1.2
-	 setb P1.2
+	 MOV P2, R4	; Third block
+	 CLR P1.2
+	 SETB P1.2
 
-	 mov P2, R5	; Fourth block
-	 clr P1.3
-	 setb P1.3
+	 MOV P2, R5	; Fourth block
+	 CLR P1.3
+	 SETB P1.3
+	 RET
+
 
 ; -------------------------------------------------
-; 	      	      DATABASE
+;	      	    DATABASES
 ;
-;	Stores bit sequences to display digits
-;             on seven-segment display
 ; -------------------------------------------------
 ORG 300h
-DB:	  db 11000000b
-	  db 11111001b, 10100100b, 10110000b
-	  db 10011001b, 10010010b, 10000010b
-	  db 11111000b, 10000000b, 10010000b
+DB_DISPLAY:	  DB 11000000b
+	  	  DB 11111001b, 10100100b, 10110000b
+	          DB 10011001b, 10010010b, 10000010b
+	          DB 11111000b, 10000000b, 10010000b
+DB_KEYPAD:        DB 00010001b, 00010010b, 00010100b	; 1, 2, 3
+		  DB 00100001b, 00100010b, 00100100b    ; 4, 5, 6
+		  DB 01000001b, 01000010b, 01000100b    ; 7, 8, 9
+		  DB 10000100b				;    0
+		  
 end
